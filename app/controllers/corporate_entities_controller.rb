1d29f59a5e55a1ae37039d038dabdb01ba2e9151
Ruan Herculano <ruannawe95@gmail.com>
2017-10-27 15:58:33 -0200

    criando controllers de pessoa física e jurídica

---
+++ app/controllers/corporate_entities_controller.rb
@@ -0,0 +1,51 @@
+class CorporateEntitiesController < ApplicationController
+  before_action :set_corporate_entity, only: [:show, :edit, :update, :destroy]
+
+  # GET /corporate_entities
+  def index
+    @corporate_entities = CorporateEntity.all
+
+    render json: @corporate_entities
+  end
+
+  # GET /corporate_entities/1
+  def show
+    render json: @corporate_entity
+  end
+
+  # POST /corporate_entities
+  def create
+    @corporate_entity = CorporateEntity.new(corporate_entity_params)
+
+    if @corporate_entity.save
+      render json: @corporate_entity, status: :created, location: @corporate_entity
+    else
+      render json: @corporate_entity.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /corporate_entities/1
+  def update
+    if @corporate_entity.update(corporate_entity_params)
+      render json: @corporate_entity
+    else
+      render json: @corporate_entity.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /corporate_entities/1
+  def destroy
+    @corporate_entity.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_corporate_entity
+      @corporate_entity = CorporateEntity.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def corporate_entity_params
+      params.require(:corporate_entity).permit(:cnpj, :business, :trading_name)
+    end
+end
---
+++ app/controllers/individual_entities_controller.rb
@@ -0,0 +1,51 @@
+class IndividualEntitiesController < ApplicationController
+  before_action :set_individual_entity, only: [:show, :edit, :update, :destroy]
+
+  # GET /individual_entities
+  def index
+    @individual_entities = IndividualEntity.all
+
+    render json: @individual_entities
+  end
+
+  # GET /individual_entities/1
+  def show
+    render json: @individual_entity
+  end
+
+  # POST /individual_entities
+  def create
+    @individual_entity = IndividualEntity.new(individual_entity_params)
+
+    if @individual_entity.save
+      render json: @individual_entity, status: :created, location: @individual_entity
+    else
+      render json: @individual_entity.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /individual_entities/1
+  def update
+    if @individual_entity.update(individual_entity_params)
+      render json: @individual_entity
+    else
+      render json: @individual_entity.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /individual_entities/1
+  def destroy
+    @individual_entity.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_individual_entity
+      @individual_entity = IndividualEntity.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def individual_entity_params
+      params.require(:individual_entity).permit(:cpf, :full_name, :date_birth)
+    end
+end

fa093e1f9bd5394b1ec84ac68a6f086060a825fe
ruanHerculano <ruannawe95@gmail.com>
2017-10-27 21:53:13 -0200

    adicionando deleção em cascata

--- db/migrate/20171027122014_create_accounts.rb
+++ db/migrate/20171027122014_create_accounts.rb
@@ -2,8 +2,8 @@ class CreateAccounts < ActiveRecord::Migration[5.1]
   def change
     create_table :accounts do |t|
       t.string :name, null: false
-      t.references :corporate_entity, foreign_key: true
-      t.references :individual_entity, foreign_key: true
+      t.references :corporate_entity, foreign_key: { on_delete: :cascade }
+      t.references :individual_entity, foreign_key: { on_delete: :cascade }
       t.references :account, foreign_key: true
       t.integer :level, null: false
       t.integer :status, null: false, default: 'active'

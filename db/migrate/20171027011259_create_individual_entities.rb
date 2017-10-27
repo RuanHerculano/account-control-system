8db9a0f30bc8e4b8b224668e5cfbd8a681e2e998
ruanHerculano <ruannawe95@gmail.com>
2017-10-26 23:39:45 -0200

    adicionando campos obrigatórios ao banco

--- db/migrate/20171027011259_create_individual_entities.rb
+++ db/migrate/20171027011259_create_individual_entities.rb
@@ -1,9 +1,9 @@
 class CreateIndividualEntities < ActiveRecord::Migration[5.1]
   def change
     create_table :individual_entities do |t|
-      t.string :cpf
-      t.string :full_name
-      t.date :date_birth
+      t.string :cpf,       null: false
+      t.string :full_name, null: false
+      t.date :date_birth,  null: false
 
       t.timestamps
     end
--- db/migrate/20171027011730_create_corporate_entities.rb
+++ db/migrate/20171027011730_create_corporate_entities.rb
@@ -1,9 +1,9 @@
 class CreateCorporateEntities < ActiveRecord::Migration[5.1]
   def change
     create_table :corporate_entities do |t|
-      t.string :cnpj
-      t.string :business
-      t.string :trading_name
+      t.string :cnpj,         null: false
+      t.string :business,     null: false
+      t.string :trading_name, null: false
 
       t.timestamps
     end

d09ba53456112f7b4121b78e64dab8bb98b24ff8
ruanHerculano <ruannawe95@gmail.com>
2017-10-28 23:41:42 -0200

    colocando valor default para level

--- db/migrate/20171027122014_create_accounts.rb
+++ db/migrate/20171027122014_create_accounts.rb
@@ -5,7 +5,7 @@ class CreateAccounts < ActiveRecord::Migration[5.1]
       t.references :corporate_entity, foreign_key: { on_delete: :cascade }
       t.references :individual_entity, foreign_key: { on_delete: :cascade }
       t.references :account, foreign_key: { on_delete: :cascade }
-      t.integer :level, null: false
+      t.integer :level, default: 0, null: false
       t.integer :status, null: false, default: 'active'
 
       t.timestamps

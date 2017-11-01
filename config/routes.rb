e0611b0fa1dde79cfa1b979c7abe6ae132093bf0
Ruan Herculano <ruannawe95@gmail.com>
2017-11-01 17:46:23 -0200

    criando rotas para contribuição financeira

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   resources :individual_entities
   resources :corporate_entities
   resources :accounts
-  resources :financial_contributions
 
   get  '/financial_transactions'     => 'financial_transactions#index'
   post '/financial_transactions'     => 'financial_transactions#create'
   put  '/financial_transactions/:id' => 'financial_transactions#reversal'
+
+  get  '/financial_contributions'     => 'financial_contributions#index'
+  post '/financial_contributions'     => 'financial_contributions#create'
+  put  '/financial_contributions/:id' => 'financial_contributions#reversal'
 end

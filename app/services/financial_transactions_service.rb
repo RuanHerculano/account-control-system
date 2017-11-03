4e6b0d7fc8744d00c073376dad1ee6bd9a1b7b08
ruanHerculano <ruannawe95@gmail.com>
2017-11-03 21:59:41 -0200

    refatorando service de transação

--- app/services/financial_transactions_service.rb
+++ app/services/financial_transactions_service.rb
@@ -1,21 +1,58 @@
 class FinancialTransactionsService
   def self.create(financial_transaction_params)
-    success = false
-    status = :unprocessable_entity
     financial_transaction_params[:code] = generate_unique_code
     financial_transaction = FinancialTransaction.new(financial_transaction_params)
     origin_account = Account.find(financial_transaction.origin_id)
 
-    if origin_account.value > financial_transaction.value
-      subtract_origin(origin_account, financial_transaction)
-      increment_destination(financial_transaction)
-      success = financial_transaction.save
-      status = :created if success
-    else
-      financial_transaction.errors.add(:value, 'origin account has insufficient value for this transaction')
+    result = validations_transaction(origin_account, financial_transaction)
+    unless result.success
+      return ResultResponseService.new(result.success, result.status, result.response)
     end
 
+    management_transaction
+  end
+
+  def self.validations_transaction(origin_account, financial_transaction)
+    active = check_active_account(origin_account, financial_transaction)
+    unless active.success
+      return ResultResponseService.new(active.success, active.status, active.response)
+    end
+
+    positive = check_positive_balance(origin_account, financial_transaction)
+    unless positive.success
+      return ResultResponseService.new(positive.success, positive.status, positive.response)
+    end
+
+    ResultResponseService.new(true, nil, nil)
+  end
+
+  def self.management_transaction(origin_account, financial_transaction)
+    success = false
+    status = :unprocessable_entity
+    subtract_origin(origin_account, financial_transaction)
+    increment_destination(financial_transaction)
+    success = financial_transaction.save
+    status = :created if success
+
     ResultResponseService.new(success, status, financial_transaction)
+  end
+
+  def self.check_active_account(origin_account, financial_transaction)
+    if origin_account.status != 'active'
+      financial_transaction.errors.add(:status, 'transfers need to be for active accounts')
+      return ResultResponseService.new(false, :unprocessable_entity, financial_transaction)
+    end
+
+    ResultResponseService.new(true, nil, nil)
+  end
+
+  def self.check_positive_balance(origin_account, financial_transaction)
+    if origin_account.value < financial_transaction.value
+      financial_transaction.errors.add(:value, 'origin account has insufficient value for this transaction')
+      return ResultResponseService.new(false, :unprocessable_entity, financial_transaction)
+    end
+
+    ResultResponseService.new(true, nil, nil)
   end
 
   def self.reversal(id)
